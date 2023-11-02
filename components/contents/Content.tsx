131eaeb78fcc2560ebd2dc407bb3c24f1d41cf8d
Fardin Mustaque <fardindeveloper1@gmail.com>
2023-11-02 19:35:16 +0530

    animation added

--- components/contents/Content.tsx
+++ components/contents/Content.tsx
@@ -4,6 +4,13 @@ import Styles from "./content.module.css";
 import Head from "next/head";
 import { Style } from "@mui/icons-material";
 import CountUp, { useCountUp } from 'react-countup';
+import { motion } from "framer-motion";
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+
+
+
 
 
 
@@ -14,6 +21,23 @@ const Content = () => {
     //     enableScrollSpy: true,
     //     scrollSpyDelay: 4000,
     // });
+    const [hasScrolled, setHasScrolled] = useState(false);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (!hasScrolled) {
+                setHasScrolled(true);
+
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [hasScrolled]);
 
     return (
 
@@ -24,48 +48,65 @@ const Content = () => {
                     {/* <p>Google Developer Student Club Leads are passionate leaders at their Universities.</p> */}
                 </div>
 
+
                 {
                     contentData?.map((item, index) => (
+
+
                         <div className={`${Styles.wrapper} ${index % 2 == 0 ? Styles.right : Styles.left}`} key={index}>
+                            <motion.div initial={{ opacity: 0, x: -100 }}
+                                whileInView={{ opacity: 1, x: 0 }}
+                                transition={{ delay: 0.9, duration: 3 }}
+                                viewport={{ once: true }}>
 
-                            <div className={Styles.image}>
-                                <img src={item.imgUrl} alt={item.title} />
-                            </div>
-
-
-                            <div className={Styles.block}>
-                                <div className={Styles.heads}>
-                                    <div className={Styles.side}>
-
-                                        <div className={Styles.oneContainer}>
-
-                                            <div className={Styles.titlenGif}>
-                                                <div className={Styles.gifPosition}>
-                                                    <img src="/assests/content/google.gif" alt="google" />
-                                                </div>
-                                                <div className={Styles.h2div}>
-                                                    <h2>{item.title}</h2>
-                                                </div>
-                                            </div>
-                                            <h4 style={{ marginTop: "-1rem", display: "flex" }}>
-                                                <CountUp start={0} end={300} duration={4}
-                                                    enableScrollSpy
-                                                    scrollSpyDelay={500}
-                                                />
-                                                <span className={Styles.plusToggle}>+</span></h4>
-                                            <p className={Styles.desc}>{item.description}</p>
-                                        </div>
-
-
-                                    </div>
+                                <div className={Styles.image}>
+                                    <img src={item.imgUrl} alt={item.title} />
                                 </div>
+                            </motion.div>
 
-                            </div>
+                            <motion.div
+                                initial={{ opacity: 0, x: 100 }}
+                                whileInView={{ opacity: 1, x: 0 }}
+                                transition={{ delay: 1.2, duration: 2 }}
+                                viewport={{ once: true }}>
+
+                                <div className={Styles.block}>
+                                    <div className={Styles.heads}>
+                                        <div className={Styles.side}>
+
+                                            <div className={Styles.oneContainer}>
+
+                                                <div className={Styles.titlenGif}>
+                                                    <div className={Styles.gifPosition}>
+                                                        <img src="/assests/content/google.gif" alt="google" />
+                                                    </div>
+                                                    <div className={Styles.h2div}>
+                                                        <h2>{item.title}</h2>
+                                                    </div>
+                                                </div>
+                                                <h4 style={{ marginTop: "-1rem", display: "flex" }}>
+                                                    <CountUp start={0} end={300} duration={4}
+                                                        enableScrollSpy
+                                                        scrollSpyDelay={500}
+                                                    />
+                                                    <span className={Styles.plusToggle}>+</span></h4>
+                                                <p className={Styles.desc}>{item.description}</p>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </motion.div>
+
                         </div>
+
+
                     ))
                 }
 
-            </div>
+            </div >
         </>
 
     )
